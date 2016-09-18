54671cb75e9515e408b6e0c058740bc870c072af
Yoann Fleury <yoann.fleury@yahoo.com>
2016-09-18 22:37:23 +0200

    feat(front) : add first front page

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use ApiBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Role\Role;
 
 class HomeController extends Controller
@@ -12,23 +13,29 @@ class HomeController extends Controller
     /**
      * @Route("/", name="app_home")
      *
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
-        $user = new User();
-        $user->setUsername('alexbc')
-            ->setFirstName('Alexandre')
-            ->setLastName('Bouin')
-            ->setEmail('alexandre.bouin@outlook.com')
-            ->setBiography('Wow, amazing. Such wonderful')
-            ->setPassword(password_hash('admin', PASSWORD_BCRYPT))
-            ->addRole(new Role('ROLE_ADMIN'));
+        $createUser = $request->query->get('createUser');
+        if ($createUser) {
+            $em = $this->getDoctrine()->getManager();
+            $user = new User();
+            $user->setUsername('alexbc')
+                ->setFirstName('Alexandre')
+                ->setLastName('Bouin')
+                ->setEmail('alexandre.bouin@outlook.com')
+                ->setBiography('Wow, amazing. Such wonderful')
+                ->setPassword(password_hash('admin', PASSWORD_BCRYPT))
+                ->addRole(new Role('ROLE_ADMIN'));
 
-        $em->persist($user);
-        $em->flush();
+            $em->persist($user);
+            $em->flush();
+        }
 
-        return $this->render('base.html.twig');
+        return $this->render(':home:index.html.twig', [
+            'albums' => $this->getDoctrine()->getRepository('ApiBundle:Album')->findAll()
+        ]);
     }
 }
